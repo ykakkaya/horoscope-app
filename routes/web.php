8ced2927901c624029407ff6b0dca366dcf83504
ykakkaya <ykakkaya@hotmail.com>
2024-03-04 00:39:19 +0300

    middleware

--- routes/web.php
+++ routes/web.php
@@ -29,25 +29,26 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/index',[CategoryController::class,'index'])->name('category.index');
+    Route::get('/category/create',[CategoryController::class,'create'])->name('category.create');
+    Route::get('/category/edit/{id}',[CategoryController::class,'edit'])->name('category.edit');
+    Route::post('/category/store',[CategoryController::class,'store'])->name('category.store');
+    Route::post('/category/update',[CategoryController::class,'update'])->name('category.update');
+    Route::get('/category/destroy/{id}',[CategoryController::class,'destroy'])->name('category.destroy');
+
+    Route::get('/horoscop/index',[HoroscopController::class,'index'])->name('horoscop.index');
+    Route::get('/horoscop/create',[HoroscopController::class,'create'])->name('horoscop.create');
+    Route::post('/horoscope/store',[HoroscopController::class,'store'])->name('horoscop.store');
+    Route::get('/horoscop/edit/{id}',[HoroscopController::class,'edit'])->name('horoscop.edit');
+    Route::post('/horoscope/update',[HoroscopController::class,'update'])->name('horoscop.update');
+    Route::get('/horoscop/destroy/{id}',[HoroscopController::class,'destroy'])->name('horoscop.destroy');
+
+    Route::get('/comment/index',[CommentController::class,'index'])->name('comment.index');
+    Route::get('/comment/edit/{id}',[CommentController::class,'edit'])->name('comment.edit');
+    Route::post('/comment/update',[CommentController::class,'update'])->name('comment.update');
+    Route::get('/comment/destroy/{id}',[CommentController::class,'destroy'])->name('comment.destroy');
 });
 
-Route::get('/index',[CategoryController::class,'index'])->name('category.index');
-Route::get('/category/create',[CategoryController::class,'create'])->name('category.create');
-Route::get('/category/edit/{id}',[CategoryController::class,'edit'])->name('category.edit');
-Route::post('/category/store',[CategoryController::class,'store'])->name('category.store');
-Route::post('/category/update',[CategoryController::class,'update'])->name('category.update');
-Route::get('/category/destroy/{id}',[CategoryController::class,'destroy'])->name('category.destroy');
-
-Route::get('/horoscop/index',[HoroscopController::class,'index'])->name('horoscop.index');
-Route::get('/horoscop/create',[HoroscopController::class,'create'])->name('horoscop.create');
-Route::post('/horoscope/store',[HoroscopController::class,'store'])->name('horoscop.store');
-Route::get('/horoscop/edit/{id}',[HoroscopController::class,'edit'])->name('horoscop.edit');
-Route::post('/horoscope/update',[HoroscopController::class,'update'])->name('horoscop.update');
-Route::get('/horoscop/destroy/{id}',[HoroscopController::class,'destroy'])->name('horoscop.destroy');
-
-Route::get('/comment/index',[CommentController::class,'index'])->name('comment.index');
-Route::get('/comment/edit/{id}',[CommentController::class,'edit'])->name('comment.edit');
-Route::post('/comment/update',[CommentController::class,'update'])->name('comment.update');
-Route::get('/comment/destroy/{id}',[CommentController::class,'destroy'])->name('comment.destroy');
 
 require __DIR__.'/auth.php';
